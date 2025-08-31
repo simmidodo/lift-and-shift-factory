653dbd0f219fc67f38b6001858debc8129a8c52f
Simran <133814475+simmidodo@users.noreply.github.com>
2025-08-31 20:53:28 +0100

    Delete terraform directory

--- terraform/terraform/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "region"         { type = string, default = "eu-west-2" }
-variable "windows_ami_id" { type = string }                     # e.g., Windows 2022 AMI
-variable "instance_type"  { type = string, default = "t3.large" }
-variable "subnet_id"      { type = string }
-variable "sg_id"          { type = string }
